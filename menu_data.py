bfecc118dd1fa4d3fd2c757b1f0d639801666391
Mr. Anonymous <om437672684@gmail.com>
2025-11-12 11:50:19 +0500

    Create menu_data.py

---
+++ menu_data.py
@@ -0,0 +1,76 @@
+# MENU CATEGORIES
+
+categories = {
+    'FAST FOOD': 1,
+    'DESI FOOD': 2,
+    'CHINESE FOOD': 3,
+    'VEGETARIAN FOOD': 4,
+    'DRINKS': 5,
+    'FINISH ORDER': 0
+}
+
+# FAST FOOD ITEMS
+fast_food = {
+    'Chicken Tikka Pizza' : [1 , 1750],
+    'Fajita Pizza' : [2 , 1800],
+    'BBQ Pizza' : [3 , 2200],
+    'Veggie Pizza' : [4 , 1450],
+    'Afghani Tikka Pizza' : [5 , 2150],
+    'Supreme Pizza' : [6 , 2500],
+    'Cheese Lovers Pizza' : [7 , 2400],
+    'Zinger Burger' : [8 , 650],
+    'Beef Burger' : [9 , 700],
+    'Chicken Burger' : [10 , 350],
+    'Anda Shami Burger' : [11 , 250],
+    'Cheese Burger' : [13 , 750],
+    'Double Decker Burger' : [14 , 650],
+    'Chicken Roll Paratha' : [14 , 220],
+    'Beef Roll Paratha' : [15 , 280],
+    'Malai Boti Roll' : [16 , 250],
+    'Chicken Tikka Roll' : [17 , 200],
+    'Shawarma' : [18 , 250],
+    'Fried Chicken' : [19 , 350],
+    'Chicken Nuggets' : [20 , 670],
+    'Crispy Wings' : [21 , 400],
+    'Chicken Strips' : [22 , 350],
+    'Masala Fries' : [23 , 200],
+    'Loaded Fries' : [24 , 350],
+    'Cheese Fries' : [25 , 400],
+    'Club Sandwich' : [26 , 250],
+    'Chicken Sandwich' : [27 , 280],
+    'Grilled Sandwich' : [28 , 340],
+    'BBQ Sandwich' : [29 , 450],
+    'Crispy Chicken Sandwich' : [30 , 400],
+    'Go back': [0, 0],
+}
+
+# DESI FOOD
+desi_food = {
+    'Chicken Karahi': [1, 1200],
+    'Mutton Karahi': [2, 2000],
+    'Beef Karahi': [3, 1800],
+    'Go back': [0, 0],
+}
+
+# CHINESE FOOD
+chinese_food = {
+    'Chinese Pasta': [1, 900],
+    'Chowmein': [2, 800],
+    'Chinese Rice': [3, 750],
+    'Go back': [0, 0]
+}
+
+# VEGETARIAN FOOD
+vegetarian_food = {
+    'Mix Salad': [1, 400],
+    'Go back': [0, 0]
+}
+
+# DRINKS
+drinks = {
+    'Coca Cola': [1, 120],
+    'Dew': [2, 130],
+    'Sting': [3, 140],
+    'Pepsi': [4, 120],
+    'Go back': [0, 0],
+}
